64fae3a9c1d125c6a8d49184208f8c39b45b912a
Patrick Brouwer <patrick@patrickbrouwer.eu>
2020-04-21 13:14:34 +0200

    First commit Variabelen

---
+++ opdracht_002_variabelen/variabelen.py
@@ -0,0 +1,19 @@
+# 1. maak een variabele aan en stop daar de waarde `3` in
+age = 3
+
+# 2. maak een variabele aan en stop daar de waarde `"Hallo"` in
+greeting = "Hallo"
+
+# 3. maak een variabele aan en stop daar de waarde `True` in
+waarheid = True
+
+# 4. print losse variabelen uit met `print`
+print(age)
+print(greeting)
+print(waarheid)
+
+# 5. print meerdere variabelen tegelijk uit met `print`
+print(greeting, age, waarheid)
+
+# 6. print meerdere dingen tegelijk uit: variabelen én expliciete waardes, geeft dat een ander resultaat?
+print(greeting, age, waarheid, "Hoi hoi")
