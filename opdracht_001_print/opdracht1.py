9de30f40b78be3653c9cb987a8714f970b9101ac
Patrick Brouwer <patrick@patrickbrouwer.eu>
2020-04-21 13:06:55 +0200

    First commit opdracht Print

---
+++ opdracht_001_print/opdracht1.py
@@ -0,0 +1,10 @@
+# print getallen
+print(7)
+print(7, 25, 13, 80)
+
+# print strings
+print("Hallo")
+print("Hello", "World")
+
+# print een combinatie van die dingen
+print("Hoi", "Patrick", "Het is vandaag", 21, "April")
